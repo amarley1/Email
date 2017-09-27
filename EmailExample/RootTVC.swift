87dfbc16e9dd46c7eda6cb2f9b167e546ba77e53
amarley1 <31295397+amarley1@users.noreply.github.com>
2017-09-26 18:31:38 -0700

    Update RootTVC.swift

--- EmailExample/RootTVC.swift
+++ EmailExample/RootTVC.swift
@@ -79,23 +79,7 @@ class RootTVC: UITableViewController {
         return true
     }
    */
-    /*
-    if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
-        
-        if let selectedIndexPath = tableView.indexPathForSelectedRow {
-            // Update an existing meal.
-            meals[selectedIndexPath.row] = meal
-            tableView.reloadRows(at: [selectedIndexPath], with: .none)
-        }
-        else {
-            // Add a new meal.
-            let newIndexPath = IndexPath(row: meals.count, section: 0)
-            
-            meals.append(meal)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
-        }
-    }*/
-    
+
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
